ba80beceff6d509195b0157f0f90bc405282d08a
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-08 11:36:08 -0500

    add test: for #paint

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -24,10 +24,6 @@ RSpec.describe Car do
     it 'has a loan length' do
       expect(car.loan_length).to eq(36)
     end
-
-    it 'has color' do
-      expect(car.color).to eq(nil)
-    end
   end
   describe '#total_cost' do
     it 'has a total cost' do
@@ -35,5 +31,18 @@ RSpec.describe Car do
     end
   end
 
+  describe '#color' do
+    it 'has color' do
+      expect(car.color).to eq(nil)
+    end
+  end
+
+  describe '#paint!' do
+    it 'adds color to the car' do
+      paint!(:blue)
+
+      expect(car.color).to eq(:blue)
+    end
+  end
 
 end
