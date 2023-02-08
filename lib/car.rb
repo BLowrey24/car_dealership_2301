a9b6c68740c00092c4165437566ec25da34c6f5a
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-08 11:25:01 -0500

    feat: car initializes with attributes

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,12 @@
 class Car 
-  attr_reader 
+  attr_reader :make, 
+              :model,
+              :monthly_payment,
+              :loan_length
 
   def initialize(make_model, monthly_payment, loan_length)
-
+    @make, @model = make_model.split(" ")
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
   end
 end
